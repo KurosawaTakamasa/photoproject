67417c46bb355df4ace62eb8dc94e2506faf0183
Kurosawa Takamasa <ta.kurosawa.1990@gmail.com>
2024-11-01 14:37:15 +0900

    ログインとログアウト完成

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_view
 
 app_name = 'accounts'
 
@@ -10,5 +11,13 @@ urlpatterns = [
     
     path('signup_success/',
          views.SignUpSuccessView.as_view(),
-         name = 'signup_success')
+         name = 'signup_success'),
+    
+    path('login/',
+         auth_view.LoginView.as_view(template_name='login.html'),
+         name='login'),
+    
+    path('logout/', 
+         auth_view.LogoutView.as_view(template_name='logout.html'),
+         name='logout')
 ]
