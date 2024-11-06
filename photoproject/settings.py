9c320bba26c562fab19a9b75910e4670601e09bd
Kurosawa Takamasa <ta.kurosawa.1990@gmail.com>
2024-11-06 12:31:30 +0900

    PWリセット完成

--- photoproject/settings.py
+++ photoproject/settings.py
@@ -116,3 +116,13 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+DEFAULT_FROM_EMAIL = 'ymg2465999@stu.o-hara.ac.jp'
+EMAIL_HOST = 'smtp-mail.outlook.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'ymg2465999@stu.o-hara.ac.jp'
+EMAIL_HOST_PASSWORD = 'Yamagata180509'
+EMAIL_USE_TLS = True
--- photoproject/urls.py
+++ photoproject/urls.py
@@ -1,10 +1,31 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
     path('', include('photo.urls')),
     
-    path('', include('accounts.urls'))
+    path('', include('accounts.urls')),
+    
+    path('password_reset/',
+         auth_view.PasswordResetView.as_view(
+             template_name = 'password_reset.html'),
+         name='password_reset'),
+    
+    path('password_reset/done/',
+         auth_view.PasswordResetDoneView.as_view(
+             template_name = 'password_reset_sent.html'),
+         name='password_reset_done'),
+    
+    path('sent/<uidb64>/<token>',
+         auth_view.PasswordResetConfirmView.as_view(
+             template_name = 'password_reset_form.html'),
+         name='password_reset_confirm'),
+    
+    path('reset/done/',
+         auth_view.PasswordResetCompleteView.as_view(
+             template_name = 'password_reset_done.html'),
+         name='password_reset_complete'),
 ]
